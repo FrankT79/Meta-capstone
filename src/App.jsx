9cfe1066b1da7a4f3772e3de7fdf13091afcf3e5
FrankT79 <tacsikf@gmail.com>
2025-02-05 15:00:09 +0100

    Basic Layout fo Form

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header/Header'
-import BookingForm from './components/BookingForm'
+import BookingForm from './components/BookingForm/BookingForm'
 import Footer from './components/Footer'
 
 
---
+++ src/components/BookingForm/BookingForm.jsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import styles from './BookingForm.module.css'
+
+function BookingForm() {
+  const[guests, setGuests] = React.useState(2)
+
+  return (
+    <main>
+      <h1>BOOKING</h1>
+      <div className={styles.formContainer}>
+      <form action="submit">
+        <div>
+        <label htmlFor="date" className={styles.blocky}>Date *</label>
+        <input type="date" id='date' name='date' placeholder='date' />
+        </div>
+        <div>
+            <label htmlFor="time" className={styles.blocky}>Time *</label>
+            <input type="time" id='time' name='time'/>
+        </div>
+        <div className={styles.bob}>
+          <label htmlFor="people" >People *</label>
+         
+          <h4>{guests}</h4>
+        </div>
+        <input type="range" id='people' name='people' placeholder='people' min={1} max={12} value={guests} onChange={(e)=>setGuests(e.target.value)}/>
+        <div>
+          <label htmlFor="firstName" className={styles.blocky}>First Name *</label>
+          <input type="text" id='firstName' name='firstName' placeholder='First Name' />
+        </div>
+        <div>
+          <label htmlFor="lastName" className={styles.blocky}>Last Name *</label>
+          <input type="text" id='lastName' name='lastName' placeholder='Last Name' />
+        </div>
+        <div>
+          <label htmlFor="email" className={styles.blocky}>Email *</label>
+          <input type="email" id='email' name='email'/>
+        </div>
+        <div>
+          <label htmlFor="phone" className={styles.blocky}>Phone *</label>
+          <input type="phone" id='phone' name='phone'/>
+        </div>
+        <div>
+          <label htmlFor="comments" className={styles.blocky}>Comments (optional)</label>
+          <textarea id='comments' name='comments' rows={4} cols={25}/>
+        </div>
+        <button type='submit'>Book Table</button>
+      </form>
+      <img src="./src/assets/restauranfood.jpg" alt="picture of restaurant food" width={300}/>
+      </div>
+    </main>
+    
+  )
+}
+
+export default BookingForm
