68d03de2dfb36c3fad24cb2d509f2efb63277c7f
FrankT79 <tacsikf@gmail.com>
2025-02-10 10:31:48 +0100

    adding API

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,82 @@
 import { useState } from 'react'
+import React from 'react'
 import Home from './Pages/Home/Home'
 import Booking from './Pages/Booking/Booking'
+import ConfirmedBooking from './components/ConfirmedBooking'
 import UnderConstruction from './Pages/UnderConstruction/UnderConstruction'
-import { Route, Routes } from 'react-router-dom'
-
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
 
 function App() {
+
+  const seededRandom = function (seed) {
+    var m = 2**35 - 31;
+    var a = 185852;
+    var s = seed % m;
+    return function () {
+        return (s = s * a % m) / m;
+    };
+}
+
+
+
+  const fetchAPI = function(date){
+    let result = [];
+    let random = seededRandom(date.getDate());
+
+    for(let i = 17; i <= 23; i++) {
+        if(random() < 0.5) {
+            result.push(i + ':00');
+        }
+        if(random() > 0.5) {
+            result.push(i + ':30');
+        }
+    }
+    return result;
+};
+const submitAPI = function(formData) {
+    return true;
+};
+
+
+const updateTimes = (availableTimes, date) => {
+  const response = fetchAPI(new Date(date));
+  return response.length !== 0 ? response : availableTimes;
+};
+
+const initializeTimes = (initialAvailableTimes) => [
+  ...initialAvailableTimes,
+  ...fetchAPI(new Date()),
+];
+
+
+  const [state, dispatchOnDateChange] = React.useReducer(
+    updateTimes,
+    [],
+    initializeTimes
+  );
+  const navigate = useNavigate();
+
+  const submitData = (formData) => {
+    const response = submitAPI(formData);
+    if (response) navigate("/confirmedBooking");
+  };
+
+
+
+
+
+
+
     
   
 
   return (
     <>
     <Routes>
-      <Route path="/Reservation" element={<Booking ></Booking>}/>
-      
+      <Route path="/Reservation" element={<Booking availableTimes = {state}  dispatchOnDateChange={dispatchOnDateChange}
+                  submitData={submitData}></Booking>}/>
+      <Route path="/confirmedBooking" element={<ConfirmedBooking></ConfirmedBooking>}/>
       </Routes>
     <UnderConstruction/>
     </>
--- src/components/BookingForm/BookingForm.jsx
+++ src/components/BookingForm/BookingForm.jsx
@@ -9,33 +9,57 @@ function BookingForm(props) {
     { value: 'Anniversary', label: 'Anniversary' },
     { value: 'engagement', label: 'Engagement' }
   ]
+
+  const options2 = [
+    'Other', 'Birthday','Anniversary', 'Engagement' 
+  ]
+
+  const optionfield = options2.map(option => <option key={option}>{option}</option>)
+  const resTimes = props.availableTimes.map(item =><option key ={item}>{item}</option>)
+
+ 
   
 
   const[date, setDate] = React.useState("");
   const[time, setTime] = React.useState("");
   const[guests, setGuests] = React.useState(2);
-  const[occasion, setOccasion] = React.useState("");
-  const[firstName, setFirstName] = React.useState("feri");
+  const[occasion, setOccasion] = React.useState(options2[0]);
+  const[firstName, setFirstName] = React.useState("");
   const[lastName, setLastName] = React.useState("");
   const[email, setEmail] = React.useState("");
   const[phone, setPhone] = React.useState("");
   const[comment, setComment] = React.useState("");
 
-  console.log(firstName)
+  const handleSubmit= (e)=>{
+    e.preventDefault();
+    props.SubmitData(e);
+  }
+  
+  const handleChange = (e)=>{
+    e.preventDefault();
+   
+    props.dispatchOnChange(e)
+  }
+
+
 
   return (
     <main>
+      <p>{date}{props.time}{guests}{occasion}{firstName}{lastName}{email}{phone}{comment}</p>
       <div className={styles.container}>
       <h1>BOOKING</h1>
       <div className={styles.formContainer}>
-      <form action="submit">
+      <form action="submit" onSubmit={handleSubmit}>
         <div>
         <label htmlFor="date" className={styles.blocky}>Date *</label>
-        <input type="date" id='date' name='date' placeholder='date' value={date} onChange={setDate} />
+        <input type="date" id='date' name='date' placeholder='date' value={date} onChange={(e)=>handleChange(e.target.value)} />
         </div>
         <div>
             <label htmlFor="time" className={styles.blocky}>Time *</label>
-            <Select options={props.resTime}  value={time} onChange={(e)=> setTime(e.target.value)}/>
+            <select id='time' value={resTimes[0]} onChange={(e)=>setTime(e.target.value)}>
+          {resTimes}
+          </select>
+           
         </div>
         <div className={styles.bob}>
           <label htmlFor="people" >People *</label>
@@ -45,7 +69,9 @@ function BookingForm(props) {
         </div>
         <div>
           <label htmlFor="occasion" className={styles.blocky}>Occasion *</label>
-          <Select options={options} value={occasion} onChange={setOccasion}/>
+          <select id='occasion' value={options2[0]} onChange={(e)=>setOccasion(e.target.value)}>
+          {optionfield}
+          </select>
         </div>
         
         <div>
@@ -54,21 +80,21 @@ function BookingForm(props) {
         </div>
         <div>
           <label htmlFor="lastName" className={styles.blocky}>Last Name *</label>
-          <input type="text" id='lastName' name='lastName' placeholder='Last Name' value={lastName} onChange={setLastName}/>
+          <input type="text" id='lastName' name='lastName' placeholder='Last Name' value={lastName} onChange={(e)=> setLastName(e.target.value)}/>
         </div>
         <div>
           <label htmlFor="email" className={styles.blocky}>Email *</label>
-          <input type="email" id='email' name='email' value={email} onChange={setEmail}/>
+          <input type="email" id='email' name='email' value={email} onChange={(e)=> setEmail(e.target.value)}/>
         </div>
         <div>
           <label htmlFor="phone" className={styles.blocky}>Phone *</label>
-          <input type="phone" id='phone' name='phone' value={phone} onChange={setPhone}/>
+          <input type="phone" id='phone' name='phone' value={phone} onChange={(e)=> setPhone(e.target.value)}/>
         </div>
         <div>
           <label htmlFor="comments" className={styles.blocky}>Comments (optional)</label>
-          <textarea id='comments' name='comments' rows={4} value={comment} onChange={setComment}/>
+          <textarea id='comments' name='comments' rows={4} value={comment} onChange={(e)=> setComment(e.target.value)}/>
         </div>
-        <button type='submit' id={styles.submitbtn}>Book Table</button>
+        <button type='submit' id={styles.submitbtn} aria-label='On Click'>Book Table</button>
       </form>
       <img src="./src/assets/restauranfood.jpg" alt="picture of restaurant food" width={300}/>
       </div>
