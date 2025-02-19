268cd84c95f138bb073e148cc5fcbf1b6e7d5ca2
FrankT79 <tacsikf@gmail.com>
2025-02-19 14:06:55 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/Meta-capstone",
   plugins: [react()],
    test: {
     setupFiles: 'src/setupTests.js',
     globals: true,
     environment: "jsdom",
-     base: "/Meta-capstone"
+     
   },
 })
