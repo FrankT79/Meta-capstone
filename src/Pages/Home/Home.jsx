95c6a8e19de5bc36edaba6de9ea5152ed6210b71
FrankT79 <tacsikf@gmail.com>
2025-02-25 18:34:50 +0100

    changes

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import Header from '../../components/Header/Header'
 import Hero from '../../components/Hero/Hero'
-import Footer from '../../components/Footer/Footer'
 import Testimonials from '../../components/Testimonials/Testimonials'
 import About from '../../components/About/About'
-
 import Card from '../../components/Card/Card'
 
 function Home() {
