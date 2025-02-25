b8d85bb8a11538db579118533159f662ed8f4ad8
FrankT79 <tacsikf@gmail.com>
2025-02-25 18:28:03 +0100

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,7 +5,7 @@ import styles from './Header.module.css'
 function Header() {
   return (
     <header>
-<img src="./src/assets/logo.svg" alt="Little Lemon Logo" />
+<img src="../src/assets/logo.svg" alt="Little Lemon Logo" />
 <Nav></Nav>
 
 </header>
