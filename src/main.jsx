9926b5dda33c85e99a82f5ba04d283f223485f6e
FrankT79 <tacsikf@gmail.com>
2025-02-25 19:17:43 +0100

    greek salad

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <BrowserRouter basename='/Meta-capstone/'>
+    <BrowserRouter basename='/Meta-capstone'>
     <App />
     </BrowserRouter>
   </StrictMode>,
